317b4e16021a4c1bf14fc5b691609044871b5fb1
Bhushan <80047431+bhushan2611@users.noreply.github.com>
2024-01-29 21:49:38 +0530

    changed to diamond pattern

--- pattern.cpp
+++ pattern.cpp
@@ -2,13 +2,36 @@
 using namespace std;
 
 int main(){
-    cout<<"enter value of n";
+    cout<<"enter value of n";      // diamond pattern
     int n;cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(j <= n-i)
+                cout<<" ";
+            else    
+                cout<<" *";
+        }
+        cout<<endl;
+    }
+    for(i=1;i<n;i++){
+        for(j=1;j<=n;j++){
+            if(j <= i)
+                cout<<" ";
+            else    
+                cout<<" *";
         }
         cout<<endl;
     }
     return 0;
 }
+
+/*
+        *
+       * *
+      * * *
+     * * * *
+      * * * 
+       * *
+        * 
+*/
